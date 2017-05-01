22171800da667c3e694c22c9c422517e4eef7dbf
Jedeu <jed.piezas@gmail.com>
2017-04-30 17:40:15 -0700

    Add scroll to projects and scroll to top functionality:

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,16 @@
 $(document).ready(function() {
+
+    $(".downarrow-container").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#projects").offset().top
+        }, 2000);
+    });
+
+    $(".uparrow-container").click(function() {
+        $('html, body').animate({
+            scrollTop: $(".home").offset().top
+        }, 2000);
+    });
     
     $(".hamburger-menu").click(function() {
         $(this).toggleClass("is-active");
